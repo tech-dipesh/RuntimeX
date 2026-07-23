be3b706d0ecdc938dec78540565f7366a5abb8b6
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-07-23 08:30:39 +0530

    Add the Redis Setup, with IMplement a Connect to a redis

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 import { z } from 'zod';
 const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.url(),
   // NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
   RATE_LIMIT: z.coerce.number().min(1)
 });
