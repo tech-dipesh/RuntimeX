3b4ecb89c0e5d10004ed9b51ba85f262f88632be
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-07-31 19:06:17 +0530

    Use the infer type as we dont' knwo the what the type will be with Making a Extend form, with Record form to select onyl ythat is valid

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,15 +1,27 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
+import { z } from 'zod';
 import { ZodError, ZodSchema } from "zod";
 
-interface SchemaValidation {
-  params?: ZodSchema;
-  query?: ZodSchema;
-  body?: ZodSchema;
+export interface SchemaValidation<
+  P extends z.ZodTypeAny = z.ZodTypeAny,
+  Q extends z.ZodTypeAny = z.ZodTypeAny,
+  B extends z.ZodTypeAny = z.ZodTypeAny
+> {
+  params?: P;
+  query?: Q;
+  body?: B;
 }
 
-
-export default function validate(schema: SchemaValidation) {
-  return (req: Request, res: Response, next: NextFunction) => {
+export default function validate<
+  P extends z.ZodTypeAny,
+  Q extends z.ZodTypeAny,
+  B extends z.ZodTypeAny
+>(schema: SchemaValidation<P, Q, B>) {
+  return (
+    req: Request<z.infer<P>, Record<string, unknown>, z.infer<B>, z.infer<Q>>,
+    res: Response,
+    next: NextFunction
+  ) => {
     if (schema.params) {
       req.params = schema.params.parse(req.params);
     }
@@ -17,8 +29,8 @@ export default function validate(schema: SchemaValidation) {
       req.query = schema.query.parse(req.query);
     }
     if (schema.body) {
-      req.body=schema.body.parse(req.body)
+      req.body = schema.body.parse(req.body);
     }
-    next()
-  }
+    next();
+  };
 }
